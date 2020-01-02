b6f5fb26e866aaadf28e9d4d7e7123f93b6a7ad9
nnngu <654015592@qq.com>
2020-01-02 08:36:03 +0800

    p206

---
+++ p206_c 语言和 cpp 的类型转换.cpp
@@ -0,0 +1,19 @@
+// int类型转换成 double 类型
+
+// 用 c 风格的类型转换
+int nVal1, nVal2;
+double fResult = ((double)nVal1) / nVal2;
+
+// cpp 风格的类型转换
+double fResult = static_cast<double>(nVal1) / nVal2;
+
+
+
+
+
+
+
+
+
+
+
