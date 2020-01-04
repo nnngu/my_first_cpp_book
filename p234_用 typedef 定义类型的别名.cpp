55b987d55dceccdc546361c388e2aa5572aef55a
nnngu <654015592@qq.com>
2020-01-05 02:45:26 +0800

    p234

---
+++ p234_用 typedef 定义类型的别名.cpp
@@ -0,0 +1,32 @@
+// typedef 定义类型别名的语法格式如下：
+typedef 数据类型 数据类型的别名
+
+
+// 为指针类型 int* 定义一个别名 PINT
+typedef int* PINT;
+
+// 定义一个 PINT 类型的变量，实际上就是 int* 类型的变量
+PINT pInt = NULL;
+
+
+// pInt1 是 int*类型，pInt2 是 int 类型
+int* pInt1, pInt2;
+
+// 在同一行代码中同时定义多个指针
+// 为指针类型 int* 定义一个别名 PINT
+typedef int* PINT;
+// 同时定义多个指针类型变量
+PINT pInt1, pInt2;
+
+
+
+
+
+
+
+
+
+
+
+
+
