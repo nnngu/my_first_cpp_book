6ee153c95bba664d3f7348ac89d4af25223ccbdc
nnngu <654015592@qq.com>
2020-01-05 19:11:48 +0800

    p242

---
+++ p242_统计高工资人数.cpp
@@ -0,0 +1,24 @@
+// 统计高工资人数
+const int MAX_COUNT = 10000;
+int main(int argc, char* argv[])
+{
+	// 定义保存员工信息的数组
+	int arrSalary[MAX_COUNT];
+	// 当前员工个数
+	int nCount = 0;
+	// 用户输入...
+
+	// 高工资的员工数
+	int nTotal = 0;
+	// 循环遍历数组进行统计
+	for (int i = 0; i < nCount; ++i)
+	{
+		if (arrSalary[i] > 1000)
+		{
+			++nTotal;
+		}
+	}
+	// 结果输出...
+}
+
+
