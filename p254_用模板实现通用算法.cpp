99a3d813c899c630648dc4ac2779317155db1d71
nnngu <654015592@qq.com>
2020-01-07 10:00:33 +0800

    p254

--- p254_用模板实现通用算法.cpp
+++ p254_用模板实现通用算法.cpp
@@ -32,6 +32,61 @@ private:
 };
 
 // 模板类的成员函数实现
+// 向容器中添加动作。这里的T，可以是一个基本数据类型，
+// 也可以是一个自定义类型
+template <class T>
+void actioncontainer<T>::add(T value)
+{
+	// 判断容器中的动作数目是否超过容器的容量
+	if (m_nUndoPos >= ACTION_SIZE)
+	{
+		// 如果容器已满，则调整添加的位置
+		// 将新动作添加到容器末尾
+		m_nUndoPos = ACTION_SIZE - 1;
+		// 将容器中的已有动作前移一个位置
+		for (int i = 0; i < ACTION_SIZE; ++i)
+		{
+			m_UndoAction[i] = m_UndoAction[i+1];
+		}
+	}
+	// 将新动作添加到容器中
+	m_UndoAction[m_nUndoPos++] = value;
+}
+
+// 撤销上一步动作
+template <class T>
+T actioncontainer<T>::undo()
+{
+	// 将撤销的动作复制到恢复数组中
+	m_RedoAction[m_nRedoPos++] = m_UndoAction[--m_nUndoPos];
+
+	// 返回撤销的动作
+	return m_UndoAction[m_nUndoPos];
+}
+
+// 恢复上一步动作
+template <class T>
+T actioncontainer<T>::redo()
+{
+	// 将恢复的动作复制到撤销数组中
+	m_UndoAction[m_nUndoPos++] = m_RedoAction[--m_nRedoPos];
+	// 返回恢复的动作
+	return m_RedoAction[m_nRedoPos]; 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
