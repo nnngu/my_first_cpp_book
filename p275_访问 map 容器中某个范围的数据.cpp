085d19e3899337f01670fd7f2b39f313f2d97cd8
nnngu <654015592@qq.com>
2020-01-09 23:48:39 +0800

    p275

--- p275_访问 map 容器中某个范围的数据.cpp
+++ p275_访问 map 容器中某个范围的数据.cpp
@@ -10,5 +10,13 @@ map<int, Employee>::iterator itto = mapEmployee.upper_bound(nToKey);
 if(mapEmployee.end() != itfrom && mapEmployee.end() != itto)
 {
 	// 输出范围内的所有数据
-	for(map<int,Employee>::iterator itfrom)
+	for(map<int,Employee>::iterator it = itfrom;
+		it != itto; ++it)
+	{
+		cout<<"当前员工号是："<<it->first<<endl;
+		cout<<"姓名："<<it->second.GetName()<<endl;
+		cout<<"工资："<<it->second.GetSalary()<<endl;
+	}
+	// 删除范围内的所有数据
+	mapEmployee.erase(itfrom, itto);
 }
