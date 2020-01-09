9107bc8ab80f1a27432d74b07e18dc1be192ccaf
nnngu <654015592@qq.com>
2020-01-09 10:10:07 +0800

    p272 p272_2

---
+++ p272_2_map 容器的常用函数.cpp
@@ -0,0 +1,11 @@
+/*
+
+m.insert(pair)		先将pair这个数据对插入map容器m中，然后map容器将根据这个数据对的键对其进行排序，也就是说，map容器始终是排序好的
+m.size()			返回容器m中元素的个数
+m.count(KEY)		判断KEY这个键是否在容器m中出现，如果出现则返回1；否则返回0
+m.find(KEY)			返回容器m中指向键为KEY的数据对的迭代器
+m.earse(pos)		删除容器m中pos迭代器所指向的元素
+m.empty()			判断容器m是否为空
+m.clear()			删除容器m中的所有元素
+
+*/
---
+++ p272_map 容器的使用.cpp
@@ -0,0 +1,7 @@
+// 引入map容器所在的头文件
+#include <map>
+using namespace std;
+// 创建一个map容器对象mapEmployee
+// 这个容器可以保存"int-Employee"形式的数据对
+// 其中，int是键的数据类型，Employee是值的数据类型
+map<int, Employee> mapEmployee;
