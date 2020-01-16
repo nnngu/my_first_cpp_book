719de39aa0b6a2f72a204387e49f6083f127ff55
nnngu <654015592@qq.com>
2020-01-16 10:32:28 +0800

    p315_2

--- p315_2_Lambda 表达式的复用.cpp
+++ p315_2_Lambda 表达式的复用.cpp
@@ -21,6 +21,28 @@ for_each(intlist.begin(), intlist.end(),
 
 
 
+// 可以把保存起来的 Lambda 表达式作为参数传递给函数
+// 可以接受函数对象为参数的函数
+void PrintMsg(tr1::function<void (int)> func)
+{
+	vector<int> v;
+	v.push_back(3);
+	for_each(v.begin(), v.end(), 
+		func); // 使用传递进来的Lambda表达式
+}
+
+// 使用Lambda表达式作为参数调用PrintMsg()函数
+PrintMsg(show);
+
+
+
+
+
+
+
+
+
+
 
 
 
